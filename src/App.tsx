e36b1605c1659338b745820f6592bc1a755f9272
Kieran Whiteman <kieran@kieran.co>
2022-09-23 10:00:59 +0100

    task: Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import { RecoilRoot } from "recoil";
 import SnackbarWrapper from "./components/SnackbarWrapper/SnackbarWrapper";
 import SnackbarForm from "./components/SnackbarForm/SnackbarForm";
-import { Divider } from "@mui/material";
+import { Divider, Paper } from "@mui/material";
 
 export default function App() {
   return (
     <RecoilRoot>
-      <Container maxWidth="md">
-        <Box sx={{ my: 4 }}>
+      <Container maxWidth="md" disableGutters style={{marginTop: '1rem'}}>
+        <Paper style={{padding: '1rem',}} elevation={6}>
           <Typography variant="h4" component="h1" gutterBottom>
             Snackbar with Recoil
           </Typography>
@@ -19,7 +18,7 @@ export default function App() {
           <br />
           <SnackbarForm />
           <SnackbarWrapper />
-        </Box>
+        </Paper>
       </Container>
     </RecoilRoot>
   );
